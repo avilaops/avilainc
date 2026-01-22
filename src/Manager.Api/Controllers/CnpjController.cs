8ba1ef34a4369c640cf77995eb363285856509a7
avilaops <nicolas@avilaops.com>
2026-01-22 11:56:34 +0000

    fd

--- src/Manager.Api/Controllers/CnpjController.cs
+++ src/Manager.Api/Controllers/CnpjController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Manager.Integrations.Cnpj;
+using Manager.Contracts.DTOs;
 
 namespace Manager.Api.Controllers;
 
